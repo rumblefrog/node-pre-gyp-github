7a726fd33da646ce91b8f7d33b640743ddfd4ce1
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-08-13 18:34:36 +0200

    add warning message

--- bin/node-pre-gyp-github.js
+++ bin/node-pre-gyp-github.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env node
 
-var module = require('../index.js');
+var NodePreGypGithub = require('../index.js');
 var program = require('commander');
+
+console.log("running https://github.com/OpenWebCAD/node-pre-gyp-github.git");
 
 program
 	.command('publish [options]')
 	.description('publishes the contents of .\\build\\stage\\{version} to the current version\'s GitHub release')
 	.option("-r, --release", "publish immediately, do not create draft")
 	.option("-s, --silent", "turns verbose messages off")
-	.action(function(cmd, options){
-		var opts = {},
-		x = new module();
+	.action(function(cmd, options) {
+		var opts = {};
 		opts.draft = options.release ? false : true;
 		opts.verbose = options.silent ? false : true;
-		x.publish(opts);
+
+		const nodePreGypGithub = new NodePreGypGithub();
+		nodePreGypGithub.publish(opts);
 	});
 
 program
-	.command('help','',{isDefault: true, noHelp: true})
+	.command('help', '', { isDefault: true, noHelp: true })
 	.action(function() {
 		console.log();
 		console.log('Usage: node-pre-gyp-github publish');
--- index.js
+++ index.js
@@ -5,177 +5,218 @@ var fs = require('fs');
 var mime = require("mime-types");
 var cwd = process.cwd();
 
-var verbose = true;
+var verbose = false;
 var consoleLog = function(x) {
 	return (verbose) ? console.log(x) : false;
 };
 
-function NodePreGypGithub() { }
-NodePreGypGithub.prototype.octokit = require("@octokit/rest");
-NodePreGypGithub.prototype.stage_dir = path.join(cwd, "build", "stage");
-NodePreGypGithub.prototype.init = function() {
-	var ownerRepo, hostPrefix;
+const { Octokit } = require("@octokit/rest");
 
-	this.package_json = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json')));
+class NodePreGypGithub {
 
-	if (!this.package_json.repository || !this.package_json.repository.url) {
-		throw new Error('Missing repository.url in package.json');
+	constructor() {
+		// super();
+		this.stage_dir = path.join(cwd, "build", "stage");
 	}
-	else {
-		ownerRepo = this.package_json.repository.url.match(/https?:\/\/([^\/]+)\/(.*)(?=\.git)/i);
-		if (ownerRepo) {
-			this.host = 'api.' + ownerRepo[1];
-			ownerRepo = ownerRepo[2].split('/');
-			this.owner = ownerRepo[0];
-			this.repo = ownerRepo[1];
+
+	init() {
+		console.log("init()");
+
+		var ownerRepo, hostPrefix;
+
+		this.package_json = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json')));
+
+		if (!this.package_json.repository || !this.package_json.repository.url) {
+			throw new Error('Missing repository.url in package.json');
 		}
-		else throw new Error('A correctly formatted GitHub repository.url was not found within package.json');
-	}
-
-	hostPrefix = 'https://' + this.host + '/' + this.owner + '/' + this.repo + '/releases/download/';
-	if (!this.package_json.binary || 'object' !== typeof this.package_json.binary || 'string' !== typeof this.package_json.binary.host) {
-		throw new Error('Missing binary.host in package.json');
-	}
-	else if (this.package_json.binary.host.replace('https://', 'https://api.').substr(0, hostPrefix.length) !== hostPrefix) {
-		throw new Error('binary.host in package.json should begin with: "' + hostPrefix + '"');
-	}
-
-	this.octokit = NodePreGypGithub.prototype.octokit({
-		baseUrl: 'https://' + this.host,
-		headers: {
-			"user-agent": (this.package_json.name) ? this.package_json.name : "node-pre-gyp-github"
-		}
-	});
-};
-
-NodePreGypGithub.prototype.authenticate_settings = function() {
-	var token = process.env.NODE_PRE_GYP_GITHUB_TOKEN;
-	if (!token) throw new Error(
-		//'Please specify NODE_PRE_GYP_GITHUB_TOKEN');
-		'NODE_PRE_GYP_GITHUB_TOKEN environment variable not found');
-
-	return {
-		"type": "oauth",
-		"token": token
-	};
-};
-
-NodePreGypGithub.prototype.createRelease = function(args, callback) {
-	var options = {
-		'host': this.host,
-		'owner': this.owner,
-		'repo': this.repo,
-		'tag_name': this.package_json.version,
-		'target_commitish': 'master',
-		'name': 'v' + this.package_json.version,
-		'body': this.package_json.name + ' ' + this.package_json.version,
-		'draft': true,
-		'prerelease': false
-	};
-
-	Object.keys(args).forEach(function(key) {
-		if (args.hasOwnProperty(key) && options.hasOwnProperty(key)) {
-			options[key] = args[key];
-		}
-	});
-	//xx this.octokit.authenticate(this.authenticate_settings());
-	this.octokit.repos.createRelease(options, callback);
-};
-
-NodePreGypGithub.prototype.uploadReleaseAsset = function(cfg) {
-
-	// this.octokit.authenticate(this.authenticate_settings());
-
-	const contentType= mime.contentType(cfg.fileName) || 'application/octet-stream';
-	const data= fs.readFileSync(cfg.filePath);
-
-	this.octokit.repos.uploadReleaseAsset({
-		url: this.release.upload_url,
-		owner: this.owner,
-		id: this.release.id,
-		repo: this.repo,
-		name: cfg.fileName,
-		data,
-		contentType
-	}, function(err) {
-		if (err) throw err;
-		consoleLog('Staged file ' + cfg.fileName + ' saved to ' + this.owner + '/' + this.repo + ' release ' + this.release.tag_name + ' successfully.');
-	}.bind(this));
-};
-
-NodePreGypGithub.prototype.uploadReleaseAssets = function() {
-	var asset;
-	consoleLog("Stage directory path: " + path.join(this.stage_dir));
-	fs.readdir(path.join(this.stage_dir), function(err, files) {
-		if (err) throw err;
-
-		if (!files.length) throw new Error('No files found within the stage directory: ' + this.stage_dir);
-
-		files.forEach(function(file) {
-			if (this.release && this.release.assets) {
-				asset = this.release.assets.filter(function(element, index, array) {
-					return element.name === file;
-				});
-				if (asset.length) {
-					throw new Error("Staged file " + file + " found but it already exists in release " + this.release.tag_name + ". If you would like to replace it, you must first manually delete it within GitHub.");
-				}
+		else {
+			ownerRepo = this.package_json.repository.url.match(/https?:\/\/([^\/]+)\/(.*)(?=\.git)/i);
+			if (ownerRepo) {
+				this.host = 'api.' + ownerRepo[1];
+				ownerRepo = ownerRepo[2].split('/');
+				this.owner = ownerRepo[0];
+				this.repo = ownerRepo[1];
 			}
-			consoleLog("Staged file " + file + " found. Proceeding to upload it.");
-			this.uploadReleaseAsset({
-				fileName: file,
-				filePath: path.join(this.stage_dir, file)
-			});
-		}.bind(this));
-	}.bind(this));
-};
-
-NodePreGypGithub.prototype.publish = function(options) {
-	options = (typeof options === 'undefined') ? {} : options;
-	verbose = (typeof options.verbose === 'undefined' || options.verbose) ? true : false;
-
-	if (!process.env.NODE_PRE_GYP_GITHUB_TOKEN) {
-		throw new Error("NODE_PRE_GYP_GITHUB_TOKEN environment variable not found");
-	}
-	this.init();
-
-	// this.octokit.authenticate(this.authenticate_settings());
-
-	this.octokit.repos.listReleases({
-		'owner': this.owner,
-		'repo': this.repo
-	}, function(err, data) {
-		var release;
-		if (err) throw err;
-
-		// when remote_path is set expect files to be in stage_dir / remote_path after substitution
-		if (this.package_json.binary.remote_path) {
-			options.tag_name = this.package_json.binary.remote_path.replace(/\{version\}/g, this.package_json.version);
-			this.stage_dir = path.join(this.stage_dir, options.tag_name);
-		} else {
-			// This is here for backwards compatibility for before binary.remote_path support was added in version 1.2.0.
-			options.tag_name = this.package_json.version;
+			else throw new Error('A correctly formatted GitHub repository.url was not found within package.json');
 		}
-		release = data.data.filter(function(element, index, array) {
-			return element.tag_name === options.tag_name;
+
+		hostPrefix = 'https://' + this.host + '/' + this.owner + '/' + this.repo + '/releases/download/';
+		if (!this.package_json.binary || 'object' !== typeof this.package_json.binary || 'string' !== typeof this.package_json.binary.host) {
+			throw new Error('Missing binary.host in package.json');
+		}
+		else if (this.package_json.binary.host.replace('https://', 'https://api.').substr(0, hostPrefix.length) !== hostPrefix) {
+			throw new Error('binary.host in package.json should begin with: "' + hostPrefix + '"');
+		}
+ 
+		this.octokit = this.octokit || new Octokit({
+
+			auth: process.env.NODE_PRE_GYP_GITHUB_TOKEN,
+
+			baseUrl: 'https://' + this.host,
+			headers: {
+				"user-agent": (this.package_json.name) ? this.package_json.name : "node-pre-gyp-github"
+			}
 		});
-		if (release.length === 0) {
-			this.createRelease(options, function(err, release) {
-				if (err) throw err;
-				this.release = release.data;
-				if (this.release.draft) {
-					consoleLog('Release ' + this.release.tag_name + " not found, so a draft release was created. YOU MUST MANUALLY PUBLISH THIS DRAFT WITHIN GITHUB FOR IT TO BE ACCESSIBLE.");
+	}
+
+
+	authenticate_settings() {
+		var token = process.env.NODE_PRE_GYP_GITHUB_TOKEN;
+		if (!token) throw new Error(
+			//'Please specify NODE_PRE_GYP_GITHUB_TOKEN');
+			'NODE_PRE_GYP_GITHUB_TOKEN environment variable not found');
+
+		return {
+			"type": "oauth",
+			"token": token
+		};
+	}
+
+	async createRelease(args) {
+		var options = {
+			'host': this.host,
+			'owner': this.owner,
+			'repo': this.repo,
+			'tag_name': this.package_json.version,
+			'target_commitish': 'master',
+			'name': 'v' + this.package_json.version,
+			'body': this.package_json.name + ' ' + this.package_json.version,
+			'draft': true,
+			'prerelease': false
+		};
+
+		Object.keys(args).forEach(function(key) {
+			if (args.hasOwnProperty(key) && options.hasOwnProperty(key)) {
+				options[key] = args[key];
+			}
+		});
+		//xx this.octokit.authenticate(this.authenticate_settings());
+		return this.octokit.repos.createRelease(options);
+	}
+
+	async uploadReleaseAsset(cfg) {
+
+		// this.octokit.authenticate(this.authenticate_settings());
+
+		const contentType = mime.contentType(cfg.fileName) || 'application/octet-stream';
+		const data = fs.readFileSync(cfg.filePath);
+
+		await this.octokit.repos.uploadReleaseAsset({
+			url: this.release.upload_url,
+			owner: this.owner,
+			id: this.release.id,
+			repo: this.repo,
+			name: cfg.fileName,
+			data,
+			contentType
+		});
+		consoleLog('Staged file ' + cfg.fileName + ' saved to ' + this.owner + '/' + this.repo + ' release ' + this.release.tag_name + ' successfully.');
+	}
+
+	async uploadReleaseAssets() {
+		var asset;
+
+		consoleLog("Stage directory path: " + path.join(this.stage_dir));
+
+		const pThis = this;
+
+		return new Promise((resolve, reject) => {
+
+			fs.readdir(path.join(this.stage_dir), async (err, files) => {
+
+				if (err) { return reject(err); }
+
+				if (!files.length) {
+					return reject(new Error('No files found within the stage directory: ' + this.stage_dir));
+				}
+
+				for (const file of files) {
+
+					if (pThis.release && pThis.release.assets) {
+
+						asset = pThis.release.assets.filter((element) => element.name === file);
+
+						if (asset.length) {
+							return reject(
+								new Error("Staged file " + file + " found but it already exists in release " + this.release.tag_name +
+								 ". If you would like to replace it, you must first manually delete it within GitHub."));
+						}
+					}
+					consoleLog("Staged file " + file + " found. Proceeding to upload it.");
+					await pThis.uploadReleaseAsset({
+						fileName: file,
+						filePath: path.join(pThis.stage_dir, file)
+					});
+				}
+
+				resolve();
+
+			});
+		});
+	}
+
+	async publish(options) {
+
+
+		try {
+
+			options = (typeof options === 'undefined') ? {} : options;
+			verbose = (typeof options.verbose === 'undefined' || options.verbose) ? true : false;
+
+			if (!process.env.NODE_PRE_GYP_GITHUB_TOKEN) {
+				throw new Error("NODE_PRE_GYP_GITHUB_TOKEN environment variable not found");
+			}
+			this.init();
+
+			// this.octokit.authenticate(this.authenticate_settings());
+
+			const pThis = this;
+
+			const data = await pThis.octokit.repos.listReleases({
+				'owner': this.owner,
+				'repo': this.repo
+			});
+
+			var release;
+
+
+			// when remote_path is set expect files to be in stage_dir / remote_path after substitution
+			if (pThis.package_json.binary.remote_path) {
+				options.tag_name = pThis.package_json.binary.remote_path.replace(/\{version\}/g, this.package_json.version);
+				pThis.stage_dir = path.join(pThis.stage_dir, options.tag_name);
+			} else {
+				// This is here for backwards compatibility for before binary.remote_path support was added in version 1.2.0.
+				options.tag_name = pThis.package_json.version;
+			}
+			release = data.data.filter((element, index, array) => element.tag_name === options.tag_name);
+
+			if (release.length === 0) {
+
+				const release = await pThis.createRelease(options);
+				pThis.release = release.data;
+				if (pThis.release.draft) {
+					consoleLog('Release ' + pThis.release.tag_name + " not found, so a draft release was created. YOU MUST MANUALLY PUBLISH THIS DRAFT WITHIN GITHUB FOR IT TO BE ACCESSIBLE.");
 				}
 				else {
 					consoleLog('Release ' + release.tag_name + " not found, so a new release was created and published.");
 				}
-				this.uploadReleaseAssets(this.release.upload_url);
-			}.bind(this));
+				await pThis.uploadReleaseAssets(pThis.release.upload_url);
+			}
+			else {
+				pThis.release = release[0];
+				await pThis.uploadReleaseAssets();
+			}
+		} catch (err) {
+			throw err;
+
 		}
-		else {
-			this.release = release[0];
-			this.uploadReleaseAssets();
-		}
-	}.bind(this));
-};
+	}
+}
+
+
+
+
+
+
 
 module.exports = NodePreGypGithub;
